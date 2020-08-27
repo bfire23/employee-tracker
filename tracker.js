ae819bf08b3f340ea6deaad9af578095daf11f64
Blake Arambula <bfire23@gmail.com>
2020-08-26 20:58:43 -0700

    updated tracker.js

--- tracker.js
+++ tracker.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const cTable = require("console.table")
+const cTable = require("console.table");
+const { response } = require("express");
 
 
 
@@ -23,7 +24,7 @@ connection.connect(function (err) {
 
 
 function runQuestions() {
- inquirer.prompt(
+  inquirer.prompt(
     {
       type: "rawlist",
       name: "beginQ",
@@ -38,21 +39,21 @@ function runQuestions() {
         "update employee roles"
       ]
     }
- )
+  )
 
 
-  .then(function(response) {
-    console.log(response);
+    .then(function (response) {
+      console.log(response);
 
-    switch(response.beginQ) {
-      case "view all departments":
-        viewAllDepartments();
-        break;
+      switch (response.beginQ) {
+        case "view all departments":
+          viewAllDepartments();
+          break;
 
-      case "view all employees":
-        viewAllEmployees();
-        break;
-        
+        case "view all employees":
+          viewAllEmployee();
+          break;
+
         case "view all roles":
           viewAllRoles();
           break;
@@ -72,13 +73,134 @@ function runQuestions() {
         case "update employee roles":
           updateEmployeeRoles();
           break;
-          
-    };
-  })
- 
+
+      }
+    });
 
 }
-  
+
+function viewAllDepartments() {
+  const query = "SELECT  * FROM departments";
+  connection.query(query, function (err, res) {
+    console.log("Departments Retrieved from Database");
+    console.table(response);
+  });
+
+  runQuestions();
+}
+
+
+
+function viewAllEmployee() {
+  const query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department on role.department_id = department.id;";
+  connection.query(query, function (err, res) {
+    console.log("employees Retrieved from Database");
+    console.table(response);
+
+  });
+  runQuestions();
+}
+
+
+
+function viewAllRoles() {
+  const query = "SELECT  * FROM role";
+  connection.query(query, function (err, res) {
+    console.log("Roles Retrieved from Database");
+    console.table(response);
+  });
+
+  runQuestions();
+}
+
+
+function addDepartment() {
+  inquirer.prompt(
+    {
+      type: "input",
+      name: "departName",
+      message: "Please add new department"
+    }
+  )
+
+    .then(function(response) {
+      const query = "INSERT INTO department (name) VALUES";
+      connection.query(query, [response.departName], function(err, res) {
+        if (err) { 
+        throw err;
+        }
+        console.log("Department Added");
+        console.table(response);
+      });
+      runQuestions();
+    })
+}
+
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter employee first name",
+        name: "first_name"
+      },
+      {
+        type: "input",
+        message: "Enter employee last name",
+        name: "last_name"
+      }
+    ])
+
+  .then(function(response) {
+    const query = "INSERT INTO employee SET";
+    connection.query(query, [response.first_name, response.last_name], function(err,res) {
+      if (err) {
+        throw err;
+      }
+      console.log("Added Employee");
+      console.table(response);
+    });
+    runQuestions();
+  })
+}
+
+
+
+function addRoles() {
+  inquirer.prompt(
+    {
+      type: "input",
+      name: "addTitle",
+      message: "enter employee title"
+    },
+    {
+      type: "input",
+      name: "addSalary",
+      message: "enter employee salary"
+    },
+    {
+      type: "input",
+      name: "addDepartmentId",
+      message: "enter employee department id"
+    }
+  )
+
+  .then(function(response) {
+    const query = "INSERT INTO role SET ?";
+    connection.query(query, [response.addTitle, response.addSalary, response.addDepartmentId], function(err,res) {
+      if (err) {
+        throw err;
+      }
+      console.log("Added Role");
+      console.table(response);
+    })
+    runQuestions();
+  })
+      
+}
+
+
 
 
 
